6e1ac88af2090ead3923d7e0c4d31b0cb4c6173e
waleedd32 <valid0@hotmail.com>
2024-05-31 22:04:10 +0300

    Enhance Header: add magnifying glass icon to search button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,6 +17,22 @@ function Header() {
             placeholder="Search..."
             className="bg-transparent focus:outline-none text-white placeholder-white p-2 w-24 sm:w-64 md:w-96 "
           />
+          <button className="text-white ml-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
         </form>
       </div>
     </header>
