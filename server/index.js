25751cc003a8c24ae73bc049836f88da6c3543c3
waleedd32 <valid0@hotmail.com>
2024-06-03 16:04:11 +0300

    Add MongoDB connection with mongoose and dotenv support for environment variables

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Real Estate API");
