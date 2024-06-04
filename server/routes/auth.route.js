2fc4bb9012ca1f1bd4fcb5c760de571115c19994
waleedd32 <valid0@hotmail.com>
2024-06-05 00:52:30 +0300

    Add password hashing and error handling in signup route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,13 +1,20 @@
 import express from "express";
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 const router = express.Router();
 
 router.post("/signup", async (req, res, next) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json("User created successfully!");
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
 export default router;
