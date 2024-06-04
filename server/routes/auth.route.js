571a1ff30d25cf8212e5db3b54d2a565b1b1fe9d
waleedd32 <valid0@hotmail.com>
2024-06-05 00:42:01 +0300

    Add user creation logic to signup route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,13 @@
 import express from "express";
+import User from "../models/user.model.js";
 
 const router = express.Router();
 
 router.post("/signup", async (req, res, next) => {
-  console.log(req.body);
+  const { username, email, password } = req.body;
+  const newUser = new User({ username, email, password });
+  await newUser.save();
+  res.status(201).json("User created successfully!");
 });
 
 export default router;
