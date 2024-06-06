311aa3be42dbe607da9d779a43d790020d1ec4e0
waleedd32 <valid0@hotmail.com>
2024-06-06 17:27:38 +0300

    Add custom error handler utility

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -13,7 +14,14 @@ router.post("/signup", async (req, res, next) => {
 
     res.status(201).json("User created successfully!");
   } catch (error) {
+    // Passing the error to the next middleware for centralized error handling
     next(error);
+
+    // Here two other possible ways handling error
+    // Second method: we can use a custom error handler function to create a more specific error
+    // next(errorHandler(550, "got error from function"));
+
+    // Third method: we can Handle the error response directly within the route
     // res.status(500).json(error.message);
   }
 });
