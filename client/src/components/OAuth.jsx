33288f1f1edc08193cc64603e6e0dba258e0272c
waleedd32 <valid0@hotmail.com>
2024-06-17 15:50:42 +0300

    Implement Google auth with Redux and navigation in OAuth component

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { signInSuccess } from "../redux/user/userSlice";
+import axios from "axios";
 
 function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
+
       const result = await signInWithPopup(auth, provider);
-      console.log("result: ", result);
+
+      const response = await axios.post("/server/auth/google", {
+        name: result.user.displayName,
+        email: result.user.email,
+        photo: result.user.photoURL,
+      });
+      const data = response.data;
+      dispatch(signInSuccess(data));
+      navigate("/");
+      console.log("result of Google Sign In: ", result);
     } catch (error) {
       console.log("could not sign in with google", error);
     }
