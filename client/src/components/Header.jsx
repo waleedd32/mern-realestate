81c7ec909dfab917ec3fa728858a3b6f58f57c3d
waleedd32 <valid0@hotmail.com>
2024-06-19 23:43:38 +0300

    Add user profile avatar to header with conditional rendering based on authentication state.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-blue-800 text-white shadow-lg">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -45,10 +48,18 @@ function Header() {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className="text-sm md:text-base hover:text-blue-300 hover:underline">
-              Sign in
-            </li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className="text-sm md:text-base hover:text-blue-300 hover:underline">
+                Sign in
+              </li>
+            )}
           </Link>
         </ul>
       </div>
