1cc410d91de190bb95c98ecb34251113f16d96c0
waleedd32 <valid0@hotmail.com>
2024-06-29 22:37:28 +0300

    Add verifyUser middleware for JWT-based authorization via cookies

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -10,6 +11,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO)
