c53869d5431adf538132ef91ac2f122d79578142
waleedd32 <valid0@hotmail.com>
2024-07-17 23:54:51 +0300

    Complete Listing creation route with verification

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.post("/create", verifyToken, async (req, res, next) => {
   try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
   } catch (error) {
     next(error);
   }
