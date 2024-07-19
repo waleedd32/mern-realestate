c57244c0f8974ade7531b00d2aab4233c7b0ad0d
waleedd32 <valid0@hotmail.com>
2024-07-19 22:52:00 +0300

    Add address field, checkboxes, and bedrooms input to CreateListing component

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -24,6 +24,49 @@ function CreateListing() {
             id="description"
             required
           />
+
+          <input
+            type="text"
+            placeholder="Address"
+            className="border p-3 rounded-lg"
+            id="address"
+            required
+          />
+          <div className="flex gap-6 flex-wrap">
+            <div className="flex gap-2">
+              <input type="checkbox" id="sale" className="w-5" />
+              <span>Sell</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="rent" className="w-5" />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="parking" className="w-5" />
+              <span>Parking spot</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="furnished" className="w-5" />
+              <span>Furnished</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="offer" className="w-5" />
+              <span>Offer</span>
+            </div>
+          </div>
+          <div className="flex flex-wrap gap-6">
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bedrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <p>Beds</p>
+            </div>
+          </div>
         </div>
       </form>
     </main>
