1cc434a8b90c2efa2af01e5547d875874abb7216
waleedd32 <valid0@hotmail.com>
2024-07-19 22:37:51 +0300

    Add input fields for name and description in CreateListing component

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function CreateListing() {
-  return <div>CreateListing</div>;
+  return (
+    <main>
+      <h1 className="text-3xl font-semibold text-center my-7">
+        Create a Listing
+      </h1>
+      <form>
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            id="name"
+            maxLength="62"
+            minLength="10"
+            required
+          />
+          <textarea
+            type="text"
+            placeholder="Description"
+            id="description"
+            required
+          />
+        </div>
+      </form>
+    </main>
+  );
 }
 
 export default CreateListing;
