fe0ac89acab55f8783b17a2ed0451bbf3875c986
waleedd32 <valid0@hotmail.com>
2024-07-21 22:05:50 +0300

    Add 'Create listing' button to CreateListing component

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -90,6 +90,9 @@ function CreateListing() {
               Upload
             </button>
           </div>
+          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Create listing
+          </button>
         </div>
       </form>
     </main>
