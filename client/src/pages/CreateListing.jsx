76b3d3ff00ae5feb1b9ba95fc0d0142a90e8dd99
waleedd32 <valid0@hotmail.com>
2024-08-03 14:15:30 +0300

    Enhanced handleSubmit in CreateListing component with image upload and price validation, improved error handling, and added navigation post-submission.

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -133,11 +133,30 @@ function CreateListing() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError("You must upload at least one image");
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError("Discount price must be lower than regular price");
+      setLoading(true);
+      setError(false);
 
-    await axios.post("/server/listing/create", {
-      ...formData,
-      userRef: currentUser._id,
-    });
+      const response = await axios.post("/server/listing/create", {
+        ...formData,
+        userRef: currentUser._id,
+      });
+
+      const data = response.data;
+
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
   };
 
   return (
