a88756bafe31718388763a7f552edaad43c69af5
waleedd32 <valid0@hotmail.com>
2024-08-10 23:58:12 +0300

    Update Profile component: Added link with improved styling and truncation for listing names.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -257,9 +257,22 @@ function Profile() {
             Your Listings
           </h1>
           {userListings.map((listing) => (
-            <div key={listing._id}>
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
               <Link to={`/listing/${listing._id}`}>
-                <img src={listing.imageUrls[0]} alt="listing cover" />
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
               </Link>
             </div>
           ))}
