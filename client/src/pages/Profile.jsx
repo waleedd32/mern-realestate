9096d486aced0cbaed80cdc1ff99c5575bc89c08
waleedd32 <valid0@hotmail.com>
2024-08-11 23:46:35 +0300

    Add edit button for user listings and initialize delete button in profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -274,6 +274,12 @@ function Profile() {
               >
                 <p>{listing.name}</p>
               </Link>
+              <div className="flex flex-col item-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
+              </div>
             </div>
           ))}
         </div>
