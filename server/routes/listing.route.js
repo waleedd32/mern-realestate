37ddc91ffd845350fb0a2d1e8db6497ec4e1c844
waleedd32 <valid0@hotmail.com>
2024-08-12 13:33:53 +0300

    Add delete listing route/endpoint with authorization and error handling

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -8,6 +9,25 @@ router.post("/create", verifyToken, async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/delete/:id", verifyToken, async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listings!"));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted!");
   } catch (error) {
     next(error);
   }
