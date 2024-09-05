b3c370e56cb211aa1c22bb2da46cfff6c5981b23
waleedd32 <valid0@hotmail.com>
2024-09-05 22:40:19 +0300

    Added loading and error states to Listing component.

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,26 +4,46 @@ import { useParams } from "react-router-dom";
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const params = useParams();
 
   useEffect(() => {
     const fetchListing = async () => {
       try {
+        setLoading(true);
+
         const res = await axios.get(`/server/listing/get/${params.listingId}`);
 
         const data = res.data;
         if (data.success === false) {
+          setError(true);
+          setLoading(false);
           return;
         }
         setListing(data);
-      } catch (error) {}
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
     };
     fetchListing();
-  }, []);
+  }, [params.listingId]);
 
   console.log("Listing of listing page", listing);
 
-  return <div>{listing && listing.name}</div>;
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
+      )}
+      {listing && listing.name}
+    </main>
+  );
 };
 
 export default Listing;
