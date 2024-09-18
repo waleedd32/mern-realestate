b83ae6e641357f04b0b739df3d29dbba4d14e5d5
waleedd32 <valid0@hotmail.com>
2024-09-19 00:04:36 +0300

    Add user retrieval by ID with error handling in API route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -74,6 +74,13 @@ router.get("/listings/:id", verifyToken, async (req, res, next) => {
 
 router.get("/:id", verifyToken, async (req, res, next) => {
   try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return next(errorHandler(404, "User not found!"));
+
+    const { password: pass, ...rest } = user._doc;
+
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
