e14eab076abec56c1636f65d144aea7e00bc2e69
waleedd32 <valid0@hotmail.com>
2024-09-21 22:56:39 +0300

    Enhance Contact component with message input and email link functionality

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Contact({ listing }) {
-  const [landlord, setLandlord] = useState(null);
+  // axios.defaults.withCredentials = true;
 
+  const [landlord, setLandlord] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
   useEffect(() => {
     const fetchLandlord = async () => {
       try {
@@ -18,7 +24,36 @@ function Contact({ listing }) {
     fetchLandlord();
   }, [listing.userRef]);
 
-  return <div>Contact</div>;
+  return (
+    <>
+      {" "}
+      {landlord && (
+        <div className="flex flex-col gap-2">
+          <p>
+            Contact <span className="font-semibold">{landlord.username}</span>{" "}
+            for{" "}
+            <span className="font-semibold">{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name="message"
+            id="message"
+            rows="2"
+            value={message}
+            onChange={onChange}
+            placeholder="Enter your message here..."
+            className="w-full border p-3 rounded-lg"
+          ></textarea>
+
+          <Link
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+          >
+            Send Message
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Contact;
