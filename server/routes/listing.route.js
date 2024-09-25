fa9931be3ae78bd16bce98143f618d940ced49ec
waleedd32 <valid0@hotmail.com>
2024-09-26 00:35:10 +0300

    Add sort, order, limit, and pagination functionality to listings API

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -98,14 +98,22 @@ router.get("/get", async (req, res, next) => {
 
     const searchTerm = req.query.searchTerm || "";
 
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
     const listings = await Listing.find({
       name: { $regex: searchTerm, $options: "i" },
       offer,
       furnished,
       parking,
       type,
-    });
-    return res.json({ message: "Get listings" });
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
