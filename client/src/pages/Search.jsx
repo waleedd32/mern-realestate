dd7e802fbfff39fdd0d715f6f2e291c6460be380
waleedd32 <valid0@hotmail.com>
2024-10-01 22:36:27 +0300

    Updated Search component: added 'Type' filter with checkbox and adjusted responsive layout.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Search = () => {
   return (
-    <div className="">
-      <div className="p-7 border-b-2">
-        <form>
+    <div className="flex flex-col md:flex-row">
+      <div className="p-7  border-b-2 md:border-r-2 md:min-h-screen">
+        <form className="flex flex-col gap-8">
           <div className="flex items-center gap-2">
             <label className="whitespace-nowrap">Search Term:</label>
             <input
@@ -13,6 +13,14 @@ const Search = () => {
               placeholder="Search..."
               className="border rounded-lg p-3 w-full"
             />
+          </div>
+
+          <div className="flex gap-2 flex-wrap items-center">
+            <label className="font-semibold">Type:</label>
+            <div className="flex gap-2">
+              <input type="checkbox" id="all" className="w-5" />
+              <span>Rent & Sale</span>
+            </div>
           </div>
         </form>
       </div>
