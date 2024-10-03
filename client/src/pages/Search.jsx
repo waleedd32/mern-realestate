d5269f316d0ab6d16afeb1aba518cd84f6a07a9c
waleedd32 <valid0@hotmail.com>
2024-10-04 01:03:45 +0300

    Updated Search component: improved form labels and added styled results section

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -6,7 +6,9 @@ const Search = () => {
       <div className="p-7  border-b-2 md:border-r-2 md:min-h-screen">
         <form className="flex flex-col gap-8">
           <div className="flex items-center gap-2">
-            <label className="whitespace-nowrap">Search Term:</label>
+            <label className="whitespace-nowrap font-semibold">
+              Search Term:
+            </label>
             <input
               type="text"
               id="searchTerm"
@@ -46,7 +48,7 @@ const Search = () => {
             </div>
           </div>
           <div className="flex items-center gap-2">
-            <label>Sort:</label>
+            <label className="font-semibold">Sort:</label>
             <select id="sort_order" className="border rounded-lg p-3">
               <option>Price high to low</option>
               <option>Price low to high</option>
@@ -61,7 +63,9 @@ const Search = () => {
         </form>
       </div>
       <div className="">
-        <h1>Listing results:</h1>
+        <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
+          Listing results:
+        </h1>{" "}
       </div>
     </div>
   );
