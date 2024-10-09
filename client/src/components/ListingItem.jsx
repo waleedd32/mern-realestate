647032e0c2fca606354345da4bc0448d8899b786
waleedd32 <valid0@hotmail.com>
2024-10-10 00:40:32 +0300

    Add ListingItem component with dynamic image and link to listing details

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ListingItem = () => {
-  return <div>listingitem</div>;
+const ListingItem = ({ listing }) => {
+  return (
+    <div>
+      {" "}
+      <Link to={`/listing/${listing._id}`}>
+        <img
+          src={
+            listing.imageUrls[0] ||
+            "https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg"
+          }
+          alt="listing cover"
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default ListingItem;
