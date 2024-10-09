60d3a05323459797060214faa764d68995f1fefb
waleedd32 <valid0@hotmail.com>
2024-10-10 00:51:14 +0300

    Add card layout, shadow effects, and listing name display to ListingItem component

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const ListingItem = ({ listing }) => {
   return (
-    <div>
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
       {" "}
       <Link to={`/listing/${listing._id}`}>
         <img
@@ -15,6 +15,11 @@ const ListingItem = ({ listing }) => {
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
       </Link>
+      <div className="p-3 flex flex-col gap-2 w-full">
+        <p className="truncate text-lg font-semibold text-slate-700">
+          {listing.name}
+        </p>
+      </div>
     </div>
   );
 };
