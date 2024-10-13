969c4c3aa9e54782ecb943af3539844fa9092dc7
waleedd32 <valid0@hotmail.com>
2024-10-13 23:28:30 +0300

    Added placeholders for top, swiper, and listing sections in Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* top */}
+
+      {/* swiper */}
+
+      {/* listing results for offer, sale and rent */}
+    </div>
+  );
 }
 
 export default Home;
