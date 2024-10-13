65033a917140c4a42b2e61dbfba2b113050318af
waleedd32 <valid0@hotmail.com>
2024-10-13 23:32:24 +0300

    Added headline section to Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,14 @@ function Home() {
     <div>
       {/* top */}
 
+      <div>
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span>
+          <br />
+          place with ease
+        </h1>
+      </div>
+
       {/* swiper */}
 
       {/* listing results for offer, sale and rent */}
