0e828941c8e40cb7bbbe24b4fac371faf0a60ac3
waleedd32 <valid0@hotmail.com>
2024-10-13 23:37:03 +0300

    Added property description and search link to Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -11,6 +12,13 @@ function Home() {
           <br />
           place with ease
         </h1>
+        <div>
+          Walid Estate is the best place to find your next perfect place to
+          live.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link to={"/search"}>Let's get started...</Link>
       </div>
 
       {/* swiper */}
