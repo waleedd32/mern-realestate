958725f4f9cbcaebd280dd71bbcacbaecc59864d
waleedd32 <valid0@hotmail.com>
2024-10-21 19:24:43 +0300

    Added section for displaying recent places for sale in Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -105,7 +105,7 @@ function Home() {
           ))}
       </Swiper>
       {/* listing results for offer, sale and rent */}
-      <div>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
           <div className="">
             <div className="my-3">
@@ -143,6 +143,15 @@ function Home() {
               {rentListings.map((listing) => (
                 <ListingItem listing={listing} key={listing._id} />
               ))}
+            </div>
+          </div>
+        )}
+        {saleListings && saleListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent places for sale
+              </h2>
             </div>
           </div>
         )}
