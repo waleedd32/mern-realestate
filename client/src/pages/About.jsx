d361028e8ecde3b3fc8cf5d49281267eb9bd85f6
waleedd32 <valid0@hotmail.com>
2024-10-23 00:13:34 +0300

    Add description text to About component

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,6 +6,13 @@ function About() {
       <h1 className="text-3xl font-bold mb-4 text-slate-800">
         About Walid Estate
       </h1>
+      <p className="mb-4 text-slate-700">
+        At Walid Estate, we pride ourselves on being a premier real estate
+        agency, dedicated to assisting clients with buying, selling, and renting
+        properties in sought-after locations. Our professional team works
+        diligently to ensure a seamless and enjoyable experience throughout the
+        entire process.
+      </p>
     </div>
   );
 }
