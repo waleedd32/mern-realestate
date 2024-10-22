8be66079e4ff99a726563802dec91ce8ea71de30
waleedd32 <valid0@hotmail.com>
2024-10-23 00:08:09 +0300

    Update About component layout and add title

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Walid Estate
+      </h1>
+    </div>
+  );
 }
 
 export default About;
