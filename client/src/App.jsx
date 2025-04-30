37d3441be74f70357631ae44472a7e986a6f2bbf
waleedd32 <valid0@hotmail.com>
2025-05-01 02:05:02 +0300

    added console log for test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || "";
 axios.defaults.withCredentials = true;
+console.log("BASE URL:", axios.defaults.baseURL);
 
 function App() {
   return (
